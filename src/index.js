8a82a3835499b616aaedf4f20cf4ac417ab17f5f
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-12-15 02:03:12 -0300

    create Contact

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(express.json()); // Receive Json In API
+
 app.use(routes);
 
 app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
