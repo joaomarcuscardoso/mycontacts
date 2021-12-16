4dadc58c4b4367c3e5faea81407d4505821c84a7
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-12-16 02:17:15 -0300

    Tratamento de erro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,10 @@ const app = express();
 app.use(express.json()); // Receive Json In API
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.status(500);
+});
 
 app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
